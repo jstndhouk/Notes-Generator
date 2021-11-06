27f450be17f97b79f77530df200582c4e1cf37d1
Justin Houk <jstndhouk@gmail.com>
2021-11-06 16:13:29 -0400

    notes save and append to the db file

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const util = require('util');
+const uuid = require('../public/assets/uuid');
+
+
+const readFromFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const router=require('express').Router();
+const path = require('path');
+const notesData = require('../db/db.json');
+
+router.get('/notes', (req, res) => {
+res.json(notesData)
+})
+
+router.post('/notes', (req, res) => {
+//Pushes the new data in the request body, into the array est. in line 10
+let reqBody=req.body;
+reqBody.id=uuid();
+notesData.push(reqBody)
+//Writes the file (second argument) found in the first arguement
+writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notesData))
+//Then console logs that data and response with the request body
+.then(data=>{console.log(data)})
+res.json(req.body)
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,46 @@
 const express = require('express');
+const htmlNotes = require('./routes/notes');
+const api = require('./routes/api');
+
+
 const path = require('path');
-//const { clog } = require('./middleware/clog');
+const { clog } = require('./middleware/clog');
 //const api = require('./routes/index.js');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
 
-app.use(express.static('public'))
+
+app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use('/api', api);
+app.use('/api', api);
+
+app.use(express.static('public'))
+
+app.use('/', htmlNotes)
+
+//app.get('*', (req, res) =>
+//  res.sendFile(path.join(__dirname, '/public/404.html'))
+//);
+
+//API Routes sending JSON data
+  //GET api/notes
+    // read db.json (you'll need fs)
+    // parse into json
+    // send the JSON to the front end
+    // if this is working you will see notes in the app
+  //POST /api/notes/ 
+    // make a bear bones post /api/notes route
+    // try console.log(req.body)
+    // make a new object with the text and title keys
+    // tack on a new id to that new object
+    // read the db.json, parse it into json
 
 
-app.get('/', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/index.html')));
-
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/notes.html')));
+  
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
